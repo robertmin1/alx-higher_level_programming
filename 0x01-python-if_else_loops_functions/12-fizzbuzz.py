28515ef1c050c801d29ea3b68a3297ed45e4934f
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-23 18:04:23 +0300

    Testing

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number%3==0 and number%5==0:
+            number = "FizzBuzz"
+        elif number%3==0:
+            number = "Fizz"
+        elif number%5==0:
+            number = "Buzz"
+        print("{} ".format(number), end="")
