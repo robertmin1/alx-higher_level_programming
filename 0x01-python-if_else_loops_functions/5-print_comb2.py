8ebd723c1a5ed96f010453e78d34757216282437
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-23 15:52:18 +0300

    testing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i <10:
-        print("{}{}".format(0, i), end="")
+        print("{}{}, ".format(0, i), end="")
     else:
-        print(i, end="")
+        print("{}, ".format(i), end="")
