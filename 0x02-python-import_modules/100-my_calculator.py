f4c58b7ff86199038a21417d94dd94f269b049fa
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-25 15:42:34 +0300

    Testing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add,sub,mul,div
+    from calculator_1 import add, sub, mul, div
+
     def error1():
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
+
     def error2():
         print("Unknown operator. Available operators: +, -, * and /")
         return 1
     if len(sys.argv) != 4:
         error1()
 
-    elif sys.argv[3] == "+" or sys.argv[3] == "-" or sys.argv[3] == "*" or sys.argv[3] == "/":
-        if sys.argv[3] == "+":
-            add(int(sys.argv[2], sys.argv[4]))
-        elif sys.argv[3] == "-":
-            sub(int(sys.argv[2], sys.argv[4]))
-        elif sys.argv[3] == "*":
-            mul(int(sys.argv[2], sys.argv[4]))
+    elif sys.argv[2] == "+" or sys.argv[2] == "-" or sys.argv[2] == "*" or sys.argv[2] == "/":
+        if sys.argv[2] == "+":
+            print(add(int(sys.argv[1]), int(sys.argv[3])))
+        elif sys.argv[2] == "-":
+            print(sub(int(sys.argv[1]), int(sys.argv[3])))
+        elif sys.argv[2] == "*":
+            print(mul(int(sys.argv[1]), int(sys.argv[3])))
         else:
-            div(int(sys.argv[2], sys.argv[4]))
+            print(div(int(sys.argv[1]), int(sys.argv[3])))
     else:
         error2()
+
