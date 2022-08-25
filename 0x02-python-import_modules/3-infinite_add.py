02baaefa01a797b3b1e8093217dd507e2c447fec
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-25 14:56:58 +0300

    Testing

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__ == "__main__":
     import sys
-    sum = 0
-    length = len(sys.argv)
-    for i in range(1, length):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
