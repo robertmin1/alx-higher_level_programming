e1da087607d5683663ecd37abb86c62ebafa8a0f
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-25 14:28:29 +0300

    Testing

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if len(sys.argv) == 1:
+        print("0 arguments")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(length-1))
+        for i, j in enumerate(range(1, length), 1):
+            print("{}: {}".format(i, sys.argv[j]))
