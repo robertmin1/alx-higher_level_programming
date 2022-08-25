ec7237093ef94193b73725c42a4c85f713ba008d
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-25 15:53:43 +0300

    Testing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
 
     elif sys.argv[2] == "+" or sys.argv[2] == "-" or sys.argv[2] == "*" or sys.argv[2] == "/":
         if sys.argv[2] == "+":
-            print(add(int(sys.argv[1]), int(sys.argv[3])))
+            print("{} + {} = {}".format(sys.argv[1], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
         elif sys.argv[2] == "-":
-            print(sub(int(sys.argv[1]), int(sys.argv[3])))
+            print("{} - {} = {}".format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
         elif sys.argv[2] == "*":
-            print(mul(int(sys.argv[1]), int(sys.argv[3])))
+            print("{} * {} = {}".format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
         else:
-            print(div(int(sys.argv[1]), int(sys.argv[3])))
+            print("{} / {} = {}".format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
     else:
         error2()
 
