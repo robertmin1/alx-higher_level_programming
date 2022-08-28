d23390d7425c5d6028c717c565fce6101fdebaf3
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-28 12:39:38 +0300

    Testing

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    my_list[idx] = element
+    return my_list
