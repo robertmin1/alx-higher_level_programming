c6b0fa33b3ca071f4ff2ffdffec8fb6b8ea6143e
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-28 12:22:12 +0300

    Testing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 def element_at(my_list, idx):
-    if idx > len(my_list)-1 or idx < 0:
+    if idx > len(my_list)-1 
+        return (None)
+    if idx < 0:
         return (None)
     else:
         return (my_list[idx])
