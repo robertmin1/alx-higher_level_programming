0b0ca3a8fdd938740ffbfe2ef68c9de5d51282e9
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-08-31 20:44:56 +0300

    Testing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
     new_list = list(map(lambda x: replace if x==search else x, my_list))
     return (new_list)
