c1e72519f7800b0b60583cfc914a527d259b8f82
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-12 12:39:30 +0300

    Testing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,6 @@
 def safe_print_list(my_list=[],x=0):
     try:
-        count = 0
         for i in range(0, x):
             print(my_list[i],end="")
-            count+=1
-        print("\nnb_print: {}".format(count))
     except:
         IndexError
