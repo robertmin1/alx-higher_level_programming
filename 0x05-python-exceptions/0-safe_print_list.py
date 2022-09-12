6cffd9b6e9b95928923c09e214c68446e026149c
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-12 12:46:40 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,5 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-    except IndexError:
-        return i
-    else:
-        return x
-    finally:
-        print("")
+    except:
+        IndentError
