7676d308c1d0d87c038d85bca1b529b70794c149
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-12 14:56:03 +0300

    Testing

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-
+    store = []
     switch = 0
     for i in range(list_length):
         try:
@@ -14,3 +14,10 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             switch = 1
+        finally:
+            if switch:
+                switch = 0
+                store.append(0)
+            else:
+                store.append(x)
+    return store
