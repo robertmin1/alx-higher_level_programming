52954af3ca6c595381df92da82fc363a1fdac17f
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-14 16:31:23 +0300

    Testing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,5 @@
 """Class Square with size"""
 class Square:
     """Class to create a square with it size"""
-    def __init__(self, size):
-        self._size = size
+    def __init__(self, square_size):
+        self._size = square_size
