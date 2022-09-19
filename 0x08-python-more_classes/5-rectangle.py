516a06cdbe4558f57a7dd965f436787cd68312d1
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-19 23:17:46 +0300

    Testing

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -57,3 +57,6 @@ class Rectangle:
         if self.__width is 0 or self.__height is 0:
             return 0
         return (2 * self.__width) + (2 * self.__height)
+    
+    def __del__(self):
+        print("Bye rectangle...")
