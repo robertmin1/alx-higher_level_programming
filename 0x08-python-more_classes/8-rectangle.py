cc7f6a6f57b9512cb53ab385610da149a7a01425
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-19 23:56:20 +0300

    Testing

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -40,6 +40,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+    
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1,Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2,Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if rect_1 >= rect_2:
+            return rect_1
+        else:
+            return rect_2
 
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
@@ -64,13 +75,4 @@ class Rectangle:
             return 0
         return (2 * self.__width) + (2 * self.__height)
     
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1,Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2,Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_1 >= rect_2:
-            return rect_1
-        else:
-            return rect_2
+    
