1dc42c2c50f1d8cb51215ed2867ba1720f407cfb
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-19 22:10:26 +0300

    Testing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,6 +22,8 @@ class Rectangle:
             raise (TypeError("size must be an integer"))
         elif value < 0:
             raise (ValueError("size must be >= 0"))
+        else:
+            self.__width = value
         
     @property
     def height(self):
@@ -33,3 +35,5 @@ class Rectangle:
             raise (TypeError("size must be an integer"))
         elif value < 0:
             raise (ValueError("size must be >= 0"))
+        else:
+            self.__height = value
