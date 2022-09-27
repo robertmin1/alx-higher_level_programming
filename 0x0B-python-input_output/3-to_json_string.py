4387eab49c1129591668317bc3948781b7416845
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-28 00:25:55 +0300

    Testing

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the "to_json_string" fundtion
+"""
+
 import json
 
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
-d = to_json_string(5)
-print(d)
