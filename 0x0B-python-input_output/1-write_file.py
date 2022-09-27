6c5342bebe13e03d913566be6c78f90f5ea7f292
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-28 00:06:49 +0300

    Testing

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the read_file function
+"""
+
+
+def read_file(filename="",text=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "w", encoding="utf-8") as f:
+        print(f.write(text), end="")
