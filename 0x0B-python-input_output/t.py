9521ff1668ed4e0f5206f99d4a8ae7782b34e133
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-28 00:29:12 +0300

    Testing

--- 0x0B-python-input_output/t.py
+++ 0x0B-python-input_output/t.py
@@ -3,6 +3,6 @@ import json
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
-    return json.dumps(my_obj)
+    return json.dump(my_obj,)
 d = to_json_string(5)
 print(d)
