5209b9992ea32a3fec4388fc30023ce67fe48174
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-09-27 23:50:41 +0300

    Testing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename,'r',encoding='utf-8') as file:
-        print(file.read())
+        for line in file:
+            print(line,end="")
